2eac4366c56b6b34593f2ec454ae94a22f3301fa
panha.khe <khepanha168@gmail.com>
2021-12-15 15:46:35 +0700

    Initial Commit

---
+++ src/crud.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Session
+from database import get_db
+import models
+import schemas
+
+def create_user(user: schemas.UserCreate, db: Session):
+    db_user = models.User(email=user.email, password=user.password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
---
+++ src/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Depends, HTTPException
+from typing import List
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+import crud
+# import models
+import schemas
+from database import get_db, SessionLocal, engine
+
+app = FastAPI()
+
+@app.post('/users', response_model = schemas.UserCreate)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    data = jsonable_encoder(crud.create_user(user=user, db=db))
+    return JSONResponse(data)
+
+@app.get('/users/{user_id}', response_model=List[schemas.UserCreate])
+def read_user(user_id: int ,db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id = user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
