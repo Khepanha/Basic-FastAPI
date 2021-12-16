42e0ae93b61a0043327adf00bb539295f09322b2
panha.khe <khepanha168@gmail.com>
2021-12-16 09:58:43 +0700

    fixed 500 internal server error

--- src/crud.py
+++ src/crud.py
@@ -12,3 +12,6 @@ def create_user(user: schemas.UserCreate, db: Session):
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
+
+def get_all_user(db: Session):
+    return db.query(models.User).filter().all()
--- src/main.py
+++ src/main.py
@@ -15,9 +15,16 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     data = jsonable_encoder(crud.create_user(user=user, db=db))
     return JSONResponse(data)
 
-@app.get('/users/{user_id}', response_model=List[schemas.UserCreate])
+@app.get('/users/{user_id}')
 def read_user(user_id: int ,db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id = user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+@app.get('/users')
+def read_all_user(db: Session = Depends(get_db)):
+    db_user = crud.get_all_user(db)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
